0c2e2d38c5f89eb0bd491dfe245cb32c6900577a
Savyasachi <jsavyasachi@gmail.com>
2026-06-05 20:12:07 -0700

    build: compile on modern JDK (javac 8, Clojure 1.12.5, silence sun.misc warnings)

--- project.clj
+++ project.clj
@@ -3,10 +3,14 @@
   :url "https://github.com/hyPiRion/beckon"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.12.5"]]
   :source-paths ["src/clojure"]
   :java-source-paths ["src/java"]
-  :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
+  ;; -XDignore.symbol.file silences javac's "internal proprietary API" warnings
+  ;; for sun.misc.Signal/SignalHandler, which beckon deliberately wraps and which
+  ;; have no public replacement. -Xlint:-options silences the source/target 8
+  ;; "obsolete" notice. Both are intentional; do not remove.
+  :javac-options ["-target" "8" "-source" "8" "-Xlint:-options" "-XDignore.symbol.file"]
   :deploy-branches ["stable"]
   :profiles {:dev {:plugins [[codox "0.6.4"]]
                    :codox {:sources ["src/clojure"]
