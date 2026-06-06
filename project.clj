8364fbeca04e3531c3b039aae5596c55a403b2fd
Savyasachi <jsavyasachi@gmail.com>
2026-06-05 20:16:00 -0700

    build: set release coordinate net.clojars.savya/beckon 0.2.0, drop stale codox and deploy gate

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject beckon "0.2.0-SNAPSHOT"
+(defproject net.clojars.savya/beckon "0.2.0"
   :description "Handle POSIX signals in Clojure."
-  :url "https://github.com/hyPiRion/beckon"
+  :url "https://github.com/jsavyasachi/beckon"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.12.5"]]
@@ -10,8 +10,4 @@
   ;; for sun.misc.Signal/SignalHandler, which beckon deliberately wraps and which
   ;; have no public replacement. -Xlint:-options silences the source/target 8
   ;; "obsolete" notice. Both are intentional; do not remove.
-  :javac-options ["-target" "8" "-source" "8" "-Xlint:-options" "-XDignore.symbol.file"]
-  :deploy-branches ["stable"]
-  :profiles {:dev {:plugins [[codox "0.6.4"]]
-                   :codox {:sources ["src/clojure"]
-                           :output-dir "codox"}}})
+  :javac-options ["-target" "8" "-source" "8" "-Xlint:-options" "-XDignore.symbol.file"])
